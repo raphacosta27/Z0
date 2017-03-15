579faa7a2490dd60890b055daa489ab3fb465f54
Luciano Pereira Soares <lpsoares@gmail.com>
2017-03-14 23:22:22 -0300

    ajuste da rotina de testes do Travis para os circuitos sequenciais

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ ui = VUnit.from_argv()
 lib = ui.add_library("lib")
 lib.add_source_files(join(root, "Computador","Modulos","PortasLogicas", "*.vhd"))  # source
 lib.add_source_files(join(root, "Computador","Modulos","UnidadeLogicaAritmetica", "*.vhd"))  # source
+lib.add_source_files(join(root, "Computador","Modulos","CircuitosSequenciais", "*.vhd"))   # source
 lib.add_source_files(join(root, "Computador","Testes","PortasLogicas", "*.vhd"))   # test
 lib.add_source_files(join(root, "Computador","Testes","UnidadeLogicaAritmetica", "*.vhd"))   # test
+lib.add_source_files(join(root, "Computador","Testes","CircuitosSequenciais", "*.vhd"))   # test
 ui.main()
